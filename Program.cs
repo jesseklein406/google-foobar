45459374ee6d28fb1ca6a8d2e8c0cde7254bd316
Jesse Klein <jklein08@gmail.com>
2019-06-19 10:47:04 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace googel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string candy;
+
+            Console.Write("Enter input: ");
+            candy = Console.ReadLine();
+
+            while (candy != "" && candy != null)
+            {
+                Console.WriteLine("Solution.solution(\"" + candy + "\"):");
+                Console.WriteLine(Solution.solution(candy));
+                Console.Write("Enter input: ");
+                candy = Console.ReadLine();
+            }
+        }
+    }
+
+    class Solution
+    {
+        public static int solution(string candy)
+        {
+            int length = candy.Length;
+            string repeated = candy + candy;
+
+            int possibilities = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (repeated.Substring(i, length) == candy)
+                {
+                    possibilities++;
+                }
+            }
+
+            return possibilities;
+        }
+    }
+}
